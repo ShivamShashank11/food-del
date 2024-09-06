90657fe19b183589e90acd1b9f1f7732541b78cc
SHIVAM SHASHANK <144351642+ShivamShashank11@users.noreply.github.com>
2024-09-06 14:56:34 +0530

    first commit

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,21 @@
+
+import logo22 from './logo22.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const url = 'http://localhost:4000'
+export const currency = '₹'
+
+export const assets ={
+    logo22,
+
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
+
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+import { Link, useNavigate } from 'react-router-dom';
+import { StoreContext } from '../../Context/StoreContext';
+
+// eslint-disable-next-line react/prop-types
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate('/');
+  };
+
+  return (
+    <div className='navbar'>
+      <Link to='/'><img className='navbar-logo' src={assets.logo6} alt="Logo" /></Link>
+      <ul className="navbar-menu">
+        <Link to="/" onClick={() => setMenu("home")} className={`${menu === "home" ? "active" : ""}`}>home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={`${menu === "menu" ? "active" : ""}`}>menu</a>
+        <a href='#app-download' onClick={() => setMenu("mob-app")} className={`${menu === "mob-app" ? "active" : ""}`}>mobile app</a>
+        <a href='#offers' onClick={() => setMenu("offers")} className={`${menu === "offers" ? "active" : ""} offer-link`}>offers</a>
+        <a href='#footer' onClick={() => setMenu("contact")} className={`${menu === "contact" ? "active" : ""}`}>contact us</a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search" />
+        <Link to='/cart' className='navbar-search-icon'>
+          <img src={assets.basket_icon} alt="Cart" />
+          <div className={getTotalCartAmount() > 0 ? "dot" : ""}></div>
+        </Link>
+        {!token ? 
+          <button onClick={() => setShowLogin(true)} className='sign-in-button'>Sign In</button>
+          : 
+          <div className='navbar-profile'>
+            <img src={assets.profile_icon} alt="Profile" />
+            <ul className='navbar-profile-dropdown'>
+              <li onClick={() => navigate('/myorders')}>
+                <img src={assets.bag_icon} alt="Orders" />
+                <p>Orders</p>
+              </li>
+              <hr />
+              <li onClick={logout}>
+                <img src={assets.logout_icon} alt="Logout" />
+                <p>Logout</p>
+              </li>
+            </ul>
+          </div>
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
