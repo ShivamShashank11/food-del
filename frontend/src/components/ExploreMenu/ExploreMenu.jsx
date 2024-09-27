5d332bad1c57d7b42998588b228a33c1ecb8d6ef
SHIVAM SHASHANK <144351642+ShivamShashank11@users.noreply.github.com>
2024-09-28 03:15:34 +0530

    menu change

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -5,61 +5,32 @@ import { StoreContext } from "../../Context/StoreContext";
 const ExploreMenu = ({ category, setCategory }) => {
   const { menu_list } = useContext(StoreContext);
 
-  // Split menu_list into two arrays for two rows
-  const firstRowItems = menu_list.slice(0, 8);
-  const secondRowItems = menu_list.slice(8, 16);
-
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore Our Menu</h1>
       <p className="explore-menu-text">
-        Choose from a diverse menu featuring a delectable array of dishes.
+        Discover a delightful range of dishes crafted for every palate.
       </p>
 
       <div className="explore-menu-list">
-        {/* First Row */}
-        <div className="explore-menu-list-row">
-          {firstRowItems.map((item, index) => (
-            <div
-              onClick={() =>
-                setCategory((prev) =>
-                  prev === item.menu_name ? "All" : item.menu_name
-                )
-              }
-              key={index}
-              className="explore-menu-list-item" // Removed active class
-            >
-              <img
-                src={item.menu_image}
-                alt={item.menu_name}
-                className="menu-item-image"
-              />
-              <p>{item.menu_name}</p>
-            </div>
-          ))}
-        </div>
-
-        {/* Second Row */}
-        <div className="explore-menu-list-row">
-          {secondRowItems.map((item, index) => (
-            <div
-              onClick={() =>
-                setCategory((prev) =>
-                  prev === item.menu_name ? "All" : item.menu_name
-                )
-              }
-              key={index}
-              className="explore-menu-list-item" // Removed active class
-            >
-              <img
-                src={item.menu_image}
-                alt={item.menu_name}
-                className="menu-item-image"
-              />
-              <p>{item.menu_name}</p>
-            </div>
-          ))}
-        </div>
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() =>
+              setCategory((prev) =>
+                prev === item.menu_name ? "All" : item.menu_name
+              )
+            }
+            key={index}
+            className="explore-menu-list-item"
+          >
+            <img
+              src={item.menu_image}
+              alt={item.menu_name}
+              className="menu-item-image"
+            />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
